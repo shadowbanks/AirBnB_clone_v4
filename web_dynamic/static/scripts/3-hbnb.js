3898c166c5db68c497681c20f951dbaab478af14
ikede-divine-keno <ikededivine@gmail.com>
2023-11-05 09:11:25 -0800

    3-hbnb.js

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -30,4 +30,33 @@ $(document).ready(function()
 			//console.log("failed");
 			$("div#api_status").removeClass("available");
 		});
+
+		$.ajax({
+			url: "http://localhost:5001/api/v1/places_search/",
+			type: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			data: JSON.stringify({}),
+			success: function (response) {
+				for (const r of response) {
+					const article = ['<article>',
+						'<div class="title_box">',
+						`<h2>${r.name}</h2>`,
+						`<div class="price_by_night">$${r.price_by_night}</div>`,
+						'</div>',
+						'<div class="information">',
+						`<div class="max_guest">${r.max_guest} Guest(s)</div>`,
+						`<div class="number_rooms">${r.number_rooms} Bedroom(s)</div>`,
+						`<div class="number_bathrooms">${r.number_bathrooms} Bathroom(s)</div>`,
+						'</div>',
+						'<div class="description">',
+						`${r.description}`,
+						'</div>',
+						'</article>'];
+					$('SECTION.places').append(article.join(''));
+				}
+			},
+			error: function (error) {
+				console.log(error);
+			}
+		});
 	});
